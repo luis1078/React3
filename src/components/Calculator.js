5692605f1ea53935f6a63af73c40ee8f7c63bbf1
Luis.Florencio <luisilloelpillo1078@gmail.com>
2024-07-06 22:03:46 -0500

    Third commit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,32 +5,39 @@ import Screen from "./Screen";
 const Calculator = forwardRef((props, ref) => {
   const [input, setInput] = useState('');
   const [resultShown, setResultShown] = useState(false);
+  const isOperator = (value) => {
+    return ['+', '-', '*', '/'].includes(value);
+  };
 
-  useImperativeHandle(ref, () => ({
-
+   useImperativeHandle(ref, () => ({
     addInput: (value) => {
       if (resultShown) {
-        setInput(value);
+        if (isOperator(value)) {
+          setInput(prevInput => prevInput.slice(0, -1) + value);
+        } else {
+          setInput(value);
+        }
         setResultShown(false);
       } else {
-        setInput((prevInput) => prevInput + value);
+        if (isOperator(value) && isOperator(input.slice(-1))) {
+          setInput(prevInput => prevInput.slice(0, -1) + value);
+        } else {
+          setInput((prevInput) => prevInput + value);
+        }
       }
     },
-
     clearInput: () => {
-      setInput('')
+      setInput('');
       setResultShown(false);
     },
-
-    calculateResult: () =>{
-        if (input) {
+    calculateResult: () => {
+      if (input) {
         setInput(evaluate(input).toString());
         setResultShown(true);
       } else {
         setInput('');
       }
     }
-
   }));
 
   return (
